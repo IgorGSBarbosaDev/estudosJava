e1bf1481ed835feca952c6cfd74b2f12f328a154
Igor Gabriel <igorgsbarbosadev@gmail.com>
2025-08-31 11:58:42 -0300

    exercicio 8 de Geral

---
+++ src/exercicios/atividadeGeral/At8.java
@@ -0,0 +1,8 @@
+package exercicios.atividadeGeral;
+
+public class At8 {
+    public static void main(String[] args) {
+        System.out.println(CodigoErro.NOT_FOUND.getCodigo()); // 404
+        System.out.println(CodigoErro.BAD_REQUEST.getDescricao()); // Requisição inválida
+    }
+}
